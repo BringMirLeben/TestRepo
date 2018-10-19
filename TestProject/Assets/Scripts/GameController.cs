f806219b1d9b2007b05bf486138038786fb0a884
BringMirLeben <tigereye_96@hotmail.com>
2018-10-20 00:33:41 +1000

    made game controller

---
+++ TestProject/Assets/Scripts/GameController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public List<GameObject> go;
+	// Use this for initialization
+	void Start () {
+		
+	}
+}
